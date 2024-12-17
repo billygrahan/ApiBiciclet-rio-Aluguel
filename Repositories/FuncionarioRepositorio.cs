e3d45db30dc333a26116990e2117f5ce87a9f7d4
Arthur <arthurlacerda206@gmail.com>
2024-12-17 16:07:51 -0300

    correção na interface e repositório

--- Repositories/FuncionarioRepositorio.cs
+++ Repositories/FuncionarioRepositorio.cs
@@ -1,6 +1,42 @@
-﻿namespace Aluguel.Repositories
+﻿using Aluguel.Context;
+using Aluguel.Models;
+using Aluguel.Repositories.Interfaces;
+
+namespace Aluguel.Repositories
 {
-    public class FuncionarioRepositorio
+    public class FuncionarioRepositorio : IFuncionarioRepositorio
     {
+        private readonly AppDbContext _appDbContext;
+
+
+        public FuncionarioRepositorio(AppDbContext context)
+        {
+            _appDbContext = context;
+        }
+
+        public Task<List<Funcionario>> Adicionar(Funcionario funcionario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Funcionario> Apagar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Funcionario> Atualizar(Funcionario funcionario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Funcionario> BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Funcionario> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Repositories/Interfaces/IFuncionarioRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using Aluguel.Models;
+
+namespace Aluguel.Repositories.Interfaces
+{
+    public interface IFuncionarioRepositorio
+    {
+        Task<List<Funcionario>> Adicionar(Funcionario funcionario);
+
+        Task<Funcionario> BuscarTodos();
+
+        Task<Funcionario> BuscarPorId(int id);
+
+        Task<Funcionario> Atualizar(Funcionario funcionario);
+
+        Task<Funcionario> Apagar(int id);
+    }
+}
